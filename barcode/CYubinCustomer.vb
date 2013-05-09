f1c844444bb270688793163879f9ff9e8e8321fa
yuichi-systembase <yuichi@systembase.co.jp>
2013-05-10 08:29:11 +0900

    郵便カスタマバーコードに対応
    バーコードの寸法はポイント単位で指定する

---
+++ barcode/CYubinCustomer.vb
@@ -0,0 +1,205 @@
+﻿Public Class CYubinCustomer
+    Inherits CBarcode
+
+    Private Shared CODE_CHARS() As String = _
+        {"144", _
+         "114", _
+         "132", _
+         "312", _
+         "123", _
+         "141", _
+         "321", _
+         "213", _
+         "231", _
+         "411", _
+         "414", _
+         "324", _
+         "342", _
+         "234", _
+         "432", _
+         "243", _
+         "423", _
+         "441", _
+         "111"}
+
+    Private Shared CODE_PATTERNS As New Dictionary(Of Char, String()) From _
+        {{"1", {CODE_CHARS(1), ""}}, _
+         {"2", {CODE_CHARS(2), ""}}, _
+         {"3", {CODE_CHARS(3), ""}}, _
+         {"4", {CODE_CHARS(4), ""}}, _
+         {"5", {CODE_CHARS(5), ""}}, _
+         {"6", {CODE_CHARS(6), ""}}, _
+         {"7", {CODE_CHARS(7), ""}}, _
+         {"8", {CODE_CHARS(8), ""}}, _
+         {"9", {CODE_CHARS(9), ""}}, _
+         {"0", {CODE_CHARS(0), ""}}, _
+         {"-", {CODE_CHARS(10), ""}}, _
+         {"A", {CODE_CHARS(11), CODE_CHARS(0)}}, _
+         {"B", {CODE_CHARS(11), CODE_CHARS(1)}}, _
+         {"C", {CODE_CHARS(11), CODE_CHARS(2)}}, _
+         {"D", {CODE_CHARS(11), CODE_CHARS(3)}}, _
+         {"E", {CODE_CHARS(11), CODE_CHARS(4)}}, _
+         {"F", {CODE_CHARS(11), CODE_CHARS(5)}}, _
+         {"G", {CODE_CHARS(11), CODE_CHARS(6)}}, _
+         {"H", {CODE_CHARS(11), CODE_CHARS(7)}}, _
+         {"I", {CODE_CHARS(11), CODE_CHARS(8)}}, _
+         {"J", {CODE_CHARS(11), CODE_CHARS(9)}}, _
+         {"K", {CODE_CHARS(12), CODE_CHARS(0)}}, _
+         {"L", {CODE_CHARS(12), CODE_CHARS(1)}}, _
+         {"M", {CODE_CHARS(12), CODE_CHARS(2)}}, _
+         {"N", {CODE_CHARS(12), CODE_CHARS(3)}}, _
+         {"O", {CODE_CHARS(12), CODE_CHARS(4)}}, _
+         {"P", {CODE_CHARS(12), CODE_CHARS(5)}}, _
+         {"Q", {CODE_CHARS(12), CODE_CHARS(6)}}, _
+         {"R", {CODE_CHARS(12), CODE_CHARS(7)}}, _
+         {"S", {CODE_CHARS(12), CODE_CHARS(8)}}, _
+         {"T", {CODE_CHARS(12), CODE_CHARS(9)}}, _
+         {"U", {CODE_CHARS(13), CODE_CHARS(0)}}, _
+         {"V", {CODE_CHARS(13), CODE_CHARS(1)}}, _
+         {"W", {CODE_CHARS(13), CODE_CHARS(2)}}, _
+         {"X", {CODE_CHARS(13), CODE_CHARS(3)}}, _
+         {"Y", {CODE_CHARS(13), CODE_CHARS(4)}}, _
+         {"Z", {CODE_CHARS(13), CODE_CHARS(5)}}}
+
+    Private Const START_PATTERN As String = "13"
+    Private Const STOP_PATTERN As String = "31"
+
+    Private Const CODE_MAX_SIZE As Integer = 20
+
+    Private Shared CHECK_DIGIT_PATTERNS As New Dictionary(Of String, Integer)
+
+    Private Const DPI As Integer = 72
+
+    Shared Sub New()
+        For i As Integer = 0 To CODE_CHARS.Length - 1
+            CHECK_DIGIT_PATTERNS.Add(CODE_CHARS(i), i)
+        Next i
+    End Sub
+
+    Public Function Encode(ByVal data As String) As List(Of String)
+        If data Is Nothing OrElse data.Length = 0 Then
+            Return Nothing
+        End If
+
+        validate(data)
+
+        Dim codes As New List(Of String)
+        For Each c As Char In data
+            Dim s As String() = CODE_PATTERNS(c)
+            codes.Add(s(0))
+            If Not s(1) Is String.Empty Then
+                codes.Add(s(1))
+            End If
+        Next
+
+        For i As Integer = codes.Count To CODE_MAX_SIZE
+            codes.Add(CODE_CHARS(14)) ' CC4
+        Next i
+
+        Dim ret = codes.GetRange(0, CODE_MAX_SIZE)
+        ret.Add(calcCheckDigit(ret))
+        ret.Insert(0, START_PATTERN)
+        ret.Add(STOP_PATTERN)
+
+        Return ret
+    End Function
+
+    Private Sub validate(ByVal data As String)
+        For Each c As Char In data
+            If CODE_PATTERNS(c) Is Nothing Then
+                Throw New ArgumentException("illegal data: " & data)
+            End If
+        Next
+    End Sub
+
+    Private Function calcCheckDigit(ByVal codes As List(Of String)) As String
+        Dim sum As Integer = 0
+        For Each s As String In codes
+            sum += CHECK_DIGIT_PATTERNS(s)
+        Next
+
+        Const checkNum As Integer = 19
+        Dim pos As Integer = checkNum - (sum Mod checkNum)
+        If pos = checkNum Then
+            pos = 0
+        End If
+
+        Return CODE_CHARS(pos)
+    End Function
+
+    Public Sub Render(ByVal g As Graphics, _
+                      ByVal x As Single, ByVal y As Single, ByVal w As Single, ByVal h As Single, _
+                      ByVal point As Single, ByVal data As String)
+        Render(g, x, y, w, h, point, DPI, data)
+    End Sub
+
+    Public Sub Render(ByVal g As Graphics, _
+                      ByVal x As Single, ByVal y As Single, ByVal w As Single, ByVal h As Single, _
+                      ByVal point As Single, ByVal dpi As Integer, ByVal data As String)
+        Render(g, New RectangleF(x, y, w, h), point, dpi, data)
+    End Sub
+
+    Public Sub Render(ByVal g As Graphics, _
+                      ByVal r As RectangleF, ByVal point As Single, _
+                      ByVal data As String)
+        Render(g, r, point, DPI, Data)
+    End Sub
+
+    Public Sub Render(ByVal g As Graphics, _
+                      ByVal r As RectangleF, ByVal point As Single, ByVal dpi As Integer, _
+                      ByVal data As String)
+        If point < 8.0F OrElse 11.5F < point Then
+            Throw New ArgumentException("illegal data: " & data)
+        End If
+
+        Dim longBarHeight As Single = mmToPixel(dpi, 3.6F * point / 10.0F)
+        Dim timingBarHeight As Single = mmToPixel(dpi, 1.2F * point / 10.0F)
+        Dim semilongBarHeight As Single = longBarHeight / 2.0F + timingBarHeight / 2.0F
+        Dim barWidth As Single = mmToPixel(dpi, 0.6F * point / 10.0F)
+        Dim barSpace As Single = mmToPixel(dpi, 0.6F * point / 10.0F)
+
+        Dim xPos As Single = r.X + MarginX * 2
+        Dim yTop As Single = r.Y + MarginY * 2
+        Dim xMax As Single = r.X + r.Width - MarginX * 2
+        Dim yMax As Single = r.Y + r.Height - MarginY * 2
+
+        For Each code As String In Encode(data)
+            For Each c As Char In code
+                Dim yPos As Single = yTop
+                Dim barHeight As Single = 0.0F
+                Select Case c
+                    Case "1"
+                        barHeight = longBarHeight
+                    Case "2"
+                        barHeight = semilongBarHeight
+                    Case "3"
+                        yPos = yTop + longBarHeight - semilongBarHeight
+                        barHeight = semilongBarHeight
+                    Case "4"
+                        yPos = yTop + longBarHeight - semilongBarHeight
+                        barHeight = timingBarHeight
+                    Case Else
+                        Throw New ArgumentException("illegal switch case: " & c)
+                End Select
+
+                Dim x As Single = xPos + MarginX
+                Dim y As Single = yPos + MarginY
+                If x > xMax Or y > yMax Then
+                    Exit For
+                End If
+
+                If y + barHeight > yMax Then
+                    barHeight = yMax - y
+                End If
+
+                g.FillRectangle(Brushes.Black, New RectangleF(x, y, barWidth, barHeight))
+                xPos = xPos + barWidth + barSpace
+            Next
+        Next
+    End Sub
+
+    Private Function mmToPixel(ByVal dpi As Integer, ByVal mm As Single) As Single
+        Return dpi * (mm / 25.4F)
+    End Function
+
+End Class
--- test/FrmTest.vb
+++ test/FrmTest.vb
@@ -4,6 +4,8 @@ Public Class FrmTest
 
     Private WithEvents pd As New System.Drawing.Printing.PrintDocument
 
+    Private page As Integer = 1
+
     Private Sub FrmTest_Load(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles MyBase.Load
         Dim ppd As New PrintPreviewDialog
         ppd.Document = pd
@@ -12,49 +14,64 @@ Public Class FrmTest
 
     Private Sub pd_PrintPage(ByVal sender As Object, ByVal e As System.Drawing.Printing.PrintPageEventArgs) Handles pd.PrintPage
         Dim g As Graphics = e.Graphics
-        With Nothing
-            Dim b As New CCode128
-            b.Render(g, 20, 50, 200, 50, "0123456789CODE")
-            b.Render(g, 20, 150, 200, 50, "!@#$%^&*()-")
-            b.Render(g, 20, 250, 200, 50, "[]{};':""")
-            b.WithText = False
-            b.Render(g, 20, 350, 200, 50, "abcd12!")
-        End With
-        With Nothing
-            Dim b As New CEan8
-            b.Render(g, 300, 50, 100, 50, "4901234")
-            b.Render(g, 300, 150, 100, 50, "5678901")
-            b.Render(g, 300, 250, 100, 50, "5678901")
-            b.WithText = False
-            b.Render(g, 300, 350, 100, 50, "8888888")
-        End With
-        With Nothing
-            Dim b As New CEan13
-            b.Render(g, 500, 50, 150, 50, "490123456789")
-            b.Render(g, 500, 150, 150, 50, "192205502800")
-            b.Render(g, 500, 250, 150, 50, "978488337649")
-            b.WithText = False
-            b.Render(g, 500, 350, 150, 50, "390123456789")
-        End With
-        With Nothing
-            Dim b As New CCode39
-            b.Render(g, 20, 500, 200, 50, "0123456789")
-            b.Render(g, 20, 600, 200, 50, "ABCDEFGHIJ")
-            b.GenerateCheckSum = True
-            b.Render(g, 20, 700, 200, 50, "KLMNOPQRSTU")
-            b.WithText = False
-            b.Render(g, 20, 800, 200, 50, "VWXYZ-. $/+%")
-        End With
-        With Nothing
-            Dim b As New CCodabar
-            b.Render(g, 300, 500, 150, 50, "A123456A")
-            b.Render(g, 300, 600, 150, 50, "B987653B")
-            b.GenerateCheckSum = True
-            b.Render(g, 300, 700, 150, 50, "C82-$:/.+34C")
-            b.WithText = False
-            b.Render(g, 300, 800, 150, 50, "D98-$:/.+21D")
-        End With
-        e.HasMorePages = False
+        Select Case page
+            Case 1
+                With Nothing
+                    Dim b As New CCode128
+                    b.Render(g, 20, 50, 200, 50, "0123456789CODE")
+                    b.Render(g, 20, 150, 200, 50, "!@#$%^&*()-")
+                    b.Render(g, 20, 250, 200, 50, "[]{};':""")
+                    b.WithText = False
+                    b.Render(g, 20, 350, 200, 50, "abcd12!")
+                End With
+                With Nothing
+                    Dim b As New CEan8
+                    b.Render(g, 300, 50, 100, 50, "4901234")
+                    b.Render(g, 300, 150, 100, 50, "5678901")
+                    b.Render(g, 300, 250, 100, 50, "5678901")
+                    b.WithText = False
+                    b.Render(g, 300, 350, 100, 50, "8888888")
+                End With
+                With Nothing
+                    Dim b As New CEan13
+                    b.Render(g, 500, 50, 150, 50, "490123456789")
+                    b.Render(g, 500, 150, 150, 50, "192205502800")
+                    b.Render(g, 500, 250, 150, 50, "978488337649")
+                    b.WithText = False
+                    b.Render(g, 500, 350, 150, 50, "390123456789")
+                End With
+                With Nothing
+                    Dim b As New CCode39
+                    b.Render(g, 20, 500, 200, 50, "0123456789")
+                    b.Render(g, 20, 600, 200, 50, "ABCDEFGHIJ")
+                    b.GenerateCheckSum = True
+                    b.Render(g, 20, 700, 200, 50, "KLMNOPQRSTU")
+                    b.WithText = False
+                    b.Render(g, 20, 800, 200, 50, "VWXYZ-. $/+%")
+                End With
+                With Nothing
+                    Dim b As New CCodabar
+                    b.Render(g, 300, 500, 150, 50, "A123456A")
+                    b.Render(g, 300, 600, 150, 50, "B987653B")
+                    b.GenerateCheckSum = True
+                    b.Render(g, 300, 700, 150, 50, "C82-$:/.+34C")
+                    b.WithText = False
+                    b.Render(g, 300, 800, 150, 50, "D98-$:/.+21D")
+                End With
+                e.HasMorePages = True
+                page += 1
+            Case 2
+                With Nothing
+                    Dim b As New CYubinCustomer
+                    b.Render(g, 20, 50, 300, 50, 8.0F, 100, "1234567890-")
+                    b.Render(g, 20, 150, 350, 50, 9.0F, 100, "ABCDEFGHIJKLMNOPQRST")
+                    b.Render(g, 20, 250, 400, 50, 10.0F, 100, "UVWXYZ")
+                    b.Render(g, 20, 350, 450, 50, 11.5F, 100, "024007315-10-3")
+                End With
+                e.HasMorePages = False
+                page = 1
+            Case Else
+        End Select
     End Sub
 
 End Class
